e560395c7257fda41be7232c164c6a04d55a0ba0
DdangKkong <dnjsdn8@naver.com>
2023-06-29 23:18:36 +0900

    230629 java start

---
+++ src/practice.java
@@ -0,0 +1,31 @@
+public class practice {
+    // int age = 20;
+    // int는 integer를 말하며 정수형 변수
+    // system.out.println(age);
+    // system.out.println(country);
+    // string country = "Korea";
+    // string은 문자열 데이터 변수
+
+    // 카멜(낙타) 표기법 - myName, zeroBase
+
+    // 파스칼 표기법 - MyName, ZeroBase
+
+    /*
+    Integer, System 처럼 맨 앞을 대문자로해야되는것도 있구나
+    (long)integer.MAX_VALUE + 1; 앞에 괄호해서 붙여주면 변수 변환이된다
+    float는 실수형 변수로 float floatNum = 1.27f; 처럼 숫자 뒤에 f 를 붙여줘야 float인것을 인식한다
+    double도 실수형 변수고 f는 안붙여도 된다.
+    2진수는 0b, 8진수는 0, 16진수는 0x 붙이고 뒤에 숫자를 써야한다
+    부울 자료형 boolean은 ture, false를 넣고
+    문자 자료형 char은 작은따옴표 안에 문자 하나만 써야한다.
+
+
+
+
+
+
+
+
+
+     */
+}
